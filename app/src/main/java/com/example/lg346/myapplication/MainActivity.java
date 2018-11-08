ad850dc82fe4ea0364e96cbf12644cf6a8d1a625
LudovicGrosse <lg3460@gmail.com>
2018-11-08 13:38:56 +0100

    Icone lancement

--- app/src/main/java/com/example/lg346/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lg346/myapplication/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.lg346.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("onCreate","onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button)findViewById(R.id.bouton1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activity1 = new Intent(getApplicationContext(), Activity1.class);
+                startActivity(activity1);
+            }
+        });
+
     }
 }
