d954896cb9fd43b188332b0129fef7fe4c1b6ea8
LudovicGrosse <lg3460@gmail.com>
2018-11-09 16:41:54 +0100

    Affichage json ok

--- app/src/main/java/com/example/lg346/myapplication/Activity1.java
+++ app/src/main/java/com/example/lg346/myapplication/Activity1.java
@@ -5,97 +5,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-/*
-public class Activity1 extends AppCompatActivity {
-    // Will show the string "data" that holds the results
-    TextView results;
-    // URL of object to be parsed
-    String JsonURL = "http://voyage2.corellis.eu/api/v2/homev2?lat=43.14554197717751&lon=6.00246207789145&offset=0";
-    // This string will hold the results
-    String donnees = "";
-    // Defining the Volley request queue that handles the URL request concurrently
-    RequestQueue requestQueue;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_1);
-        // Creates the Volley request queue
-        requestQueue = Volley.newRequestQueue(this);
-
-        // Casts results into the TextView found within the main layout XML with id jsonData
-        results = (TextView) findViewById(R.id.jsonData);
-        JSONArray jsonArray = new JSONArray();
-
-
-        // Creating the JsonObjectRequest class called obreq, passing required parameters:
-        //GET is used to fetch data from the server, JsonURL is the URL to be fetched from.
-        JsonArrayRequest arrayreq = new JsonArrayRequest(Request.Method.GET, JsonURL, jsonArray,
-                // The third parameter Listener overrides the method onResponse() and passes
-                //JSONObject as a parameter
-                new Response.Listener<JSONArray>() {
-
-                    // Takes the response from the JSON request
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        Log.d("MonActivity","Ceci est un test 0");
-                        try {
-                            JSONObject aux = response.getJSONObject(0);
-                            JSONArray tab = aux.getJSONArray("data");
-
-                            // Retrieves the string labeled "colorName" and "description" from
-                            //the response JSON Object
-                            //and converts them into javascript objects
-                            for (int i = 0; i < tab.length(); i++) {
-                                //gets each JSON object within the JSON array
-                                JSONObject jsonObject = tab.getJSONObject(i);
-
-                                // converts into javascript objects
-                                String type = jsonObject.getString("type");
-                                String id = jsonObject.getString("id");
-
-                                // Adds strings from the current object to the data string
-                                //spacing is included at the end to separate the results from
-                                //one another
-                                donnees += "Data " + (i + 1) + "type: " + type +  "id : " + id;
-                            }
-                            // Adds the data string to the TextView "results"
-                            results.setText(donnees);
-                        }
-                        // Try and catch are included to handle any errors due to JSON
-                        catch (JSONException e) {
-                            // If an error occurs, this prints the error to the log
-                            Log.d("MonActivity","Ceci est un test 3");
-                            e.printStackTrace();
-                        }
-                    }
-                },
-                // The final parameter overrides the method onErrorResponse() and passes VolleyError
-                //as a parameter
-                new Response.ErrorListener() {
-                    @Override
-                    // Handles errors that occur due to Volley
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e("Volley", "Error");
-                    }
-                }
-        );
-        // Adds the JSON array request "arrayreq" to the request queue
-        requestQueue.add(arrayreq);
-    }
-}*/
 
 public class Activity1 extends AppCompatActivity {
     // Will show the string "data" that holds the results
@@ -140,18 +58,15 @@ public class Activity1 extends AppCompatActivity {
 
                                 // Retrieves the string labeled "colorName" and "hexValue",
                                 // and converts them into javascript objects
-                                String type = jsonObject.getString("type");
-                                String id = jsonObject.getString("id");
+                                String type = !jsonObject.isNull("type") ? jsonObject.getString("type"): "";
+                                String id = !jsonObject.isNull("id") ? jsonObject.getString("id"): "";
 
                                 // Adds strings from the current object to the data string
                                 //spacing is included at the end to separate the results from
                                 //one another
-                                data += "Data " + (i + 1) + "Type : " + type +
-                                        "ID : " + id + "nnn";
-                                Log.d("TEST :", "eeee!");
-                            }
+                                data += "Data " + (i + 1) + ": [ Type : " + type +
+                                        ", ID : " + id + "] --- "; }
                             // Adds the data string to the TextView "results"
-                            Log.d("TEST :", "eeee");
                             results.setText(data);
                         }
                         // Try and catch are included to handle any errors due to JSON
