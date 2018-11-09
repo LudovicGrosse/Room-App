c434d5097568d85511f25cda5e8c43f941a1a893
LudovicGrosse <lg3460@gmail.com>
2018-11-09 16:10:09 +0100

    Adaptation du json dans notre cas (Etat fin AM)

--- app/src/main/java/com/example/lg346/myapplication/Activity1.java
+++ app/src/main/java/com/example/lg346/myapplication/Activity1.java
@@ -9,17 +9,99 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+/*
+public class Activity1 extends AppCompatActivity {
+    // Will show the string "data" that holds the results
+    TextView results;
+    // URL of object to be parsed
+    String JsonURL = "http://voyage2.corellis.eu/api/v2/homev2?lat=43.14554197717751&lon=6.00246207789145&offset=0";
+    // This string will hold the results
+    String donnees = "";
+    // Defining the Volley request queue that handles the URL request concurrently
+    RequestQueue requestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_1);
+        // Creates the Volley request queue
+        requestQueue = Volley.newRequestQueue(this);
+
+        // Casts results into the TextView found within the main layout XML with id jsonData
+        results = (TextView) findViewById(R.id.jsonData);
+        JSONArray jsonArray = new JSONArray();
+
+
+        // Creating the JsonObjectRequest class called obreq, passing required parameters:
+        //GET is used to fetch data from the server, JsonURL is the URL to be fetched from.
+        JsonArrayRequest arrayreq = new JsonArrayRequest(Request.Method.GET, JsonURL, jsonArray,
+                // The third parameter Listener overrides the method onResponse() and passes
+                //JSONObject as a parameter
+                new Response.Listener<JSONArray>() {
+
+                    // Takes the response from the JSON request
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("MonActivity","Ceci est un test 0");
+                        try {
+                            JSONObject aux = response.getJSONObject(0);
+                            JSONArray tab = aux.getJSONArray("data");
+
+                            // Retrieves the string labeled "colorName" and "description" from
+                            //the response JSON Object
+                            //and converts them into javascript objects
+                            for (int i = 0; i < tab.length(); i++) {
+                                //gets each JSON object within the JSON array
+                                JSONObject jsonObject = tab.getJSONObject(i);
+
+                                // converts into javascript objects
+                                String type = jsonObject.getString("type");
+                                String id = jsonObject.getString("id");
+
+                                // Adds strings from the current object to the data string
+                                //spacing is included at the end to separate the results from
+                                //one another
+                                donnees += "Data " + (i + 1) + "type: " + type +  "id : " + id;
+                            }
+                            // Adds the data string to the TextView "results"
+                            results.setText(donnees);
+                        }
+                        // Try and catch are included to handle any errors due to JSON
+                        catch (JSONException e) {
+                            // If an error occurs, this prints the error to the log
+                            Log.d("MonActivity","Ceci est un test 3");
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                // The final parameter overrides the method onErrorResponse() and passes VolleyError
+                //as a parameter
+                new Response.ErrorListener() {
+                    @Override
+                    // Handles errors that occur due to Volley
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Volley", "Error");
+                    }
+                }
+        );
+        // Adds the JSON array request "arrayreq" to the request queue
+        requestQueue.add(arrayreq);
+    }
+}*/
 
 public class Activity1 extends AppCompatActivity {
     // Will show the string "data" that holds the results
     TextView results;
     // URL of object to be parsed
-    String JsonURL = "https://raw.githubusercontent.com/ianbar20/JSON-Volley-Tutorial/master/Example-JSON-Files/Example-Object.JSON";
+    String JsonURL = "http://voyage2.corellis.eu/api/v2/homev2?lat=43.14554197717751&lon=6.00246207789145&offset=0";
     // This string will hold the results
     String data = "";
     // Defining the Volley request queue that handles the URL request concurrently
@@ -36,35 +118,47 @@ public class Activity1 extends AppCompatActivity {
         results = (TextView) findViewById(R.id.jsonData);
         JSONObject jsonObj = new JSONObject();
 
-
-        // Creating the JsonObjectRequest class called obreq, passing required parameters:
-        //GET is used to fetch data from the server, JsonURL is the URL to be fetched from.
-        JsonObjectRequest obreq = new JsonObjectRequest(Request.Method.GET, JsonURL,jsonObj,
-                // The third parameter Listener overrides the method onResponse() and passes
-                //JSONObject as a parameter
+        // Creating the JsonArrayRequest class called arrayreq, passing the required parameters
+        //JsonURL is the URL to be fetched from
+        JsonObjectRequest arrayobj = new JsonObjectRequest(JsonURL,jsonObj,
+                // The second parameter Listener overrides the method onResponse() and passes
+                //JSONArray as a parameter
                 new Response.Listener<JSONObject>() {
 
                     // Takes the response from the JSON request
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-                            JSONObject obj = response.getJSONObject("colorObject");
-                            // Retrieves the string labeled "colorName" and "description" from
-                            //the response JSON Object
-                            //and converts them into javascript objects
-                            String color = obj.getString("colorName");
-                            String desc = obj.getString("description");
+                            // Retrieves first JSON object in outer array
+                            // Retrieves "colorArray" from the JSON object
+                            JSONArray tab = response.getJSONArray("data");
+                            // Iterates through the JSON Array getting objects and adding them
+                            //to the list view until there are no more objects in colorArray
+                            for (int i = 0; i < tab.length(); i++) {
+                                //gets each JSON object within the JSON array
+                                JSONObject jsonObject = tab.getJSONObject(i);
 
-                            // Adds strings from object to the "data" string
-                            data += "Color Name: " + color +
-                                    "nDescription : " + desc;
+                                // Retrieves the string labeled "colorName" and "hexValue",
+                                // and converts them into javascript objects
+                                String type = jsonObject.getString("type");
+                                String id = jsonObject.getString("id");
 
+                                // Adds strings from the current object to the data string
+                                //spacing is included at the end to separate the results from
+                                //one another
+                                data += "Data " + (i + 1) + "Type : " + type +
+                                        "ID : " + id + "nnn";
+                                Log.d("TEST :", "eeee!");
+                            }
                             // Adds the data string to the TextView "results"
+                            Log.d("TEST :", "eeee");
                             results.setText(data);
                         }
                         // Try and catch are included to handle any errors due to JSON
                         catch (JSONException e) {
                             // If an error occurs, this prints the error to the log
+
+                            Log.d("TEST :", "aaaa"+e.toString());
                             e.printStackTrace();
                         }
                     }
@@ -79,7 +173,7 @@ public class Activity1 extends AppCompatActivity {
                     }
                 }
         );
-        // Adds the JSON object request "obreq" to the request queue
-        requestQueue.add(obreq);
+        // Adds the JSON array request "arrayreq" to the request queue
+        requestQueue.add(arrayobj);
     }
 }
