c4dacdc4e7670a94c846f9e7a2dd747dea7dd38b
LudovicGrosse <lg3460@gmail.com>
2018-11-10 20:36:50 +0100

    Selection coordonnees GPS avec gestion erreurs

--- app/src/main/java/com/example/lg346/myapplication/Destination.java
+++ app/src/main/java/com/example/lg346/myapplication/Destination.java
@@ -1,8 +1,14 @@
 package com.example.lg346.myapplication;
 
+import android.util.Log;
+
+import java.sql.SQLOutput;
+import java.util.SortedMap;
+
 public class Destination {
     private String type, title, media;
-    private float distance;
+    private double myLatitude, myLongitude;
+    private double distance;
 
     public String getType() {
         return type;
@@ -28,7 +34,7 @@ public class Destination {
         this.media = media;
     }
 
-    public float getDistance() {
+    public double getDistance() {
         return distance;
     }
 
@@ -36,10 +42,46 @@ public class Destination {
         this.distance = distance;
     }
 
-    public Destination(String type, String title, String media, float distance) {
+    public Destination(String type, String title, String media, double myLatitude, double myLongitude) {
         this.type = type;
         this.title = title;
         this.media = media;
-        this.distance = distance;
+        this.distance = 0;
+        this.myLatitude = myLatitude;
+        this.myLongitude = myLongitude;
+    }
+
+    public double getMyLatitude() {
+        return myLatitude;
+    }
+
+    public void setMyLatitude(double myLatitude) {
+        this.myLatitude = myLatitude;
+    }
+
+    public double getMyLongitude() {
+        return myLongitude;
+    }
+
+    public void setMyLongitude(double myLongitude) {
+        this.myLongitude = myLongitude;
+    }
+
+    public double calculDistance(double latitude, double longitude){
+        //double latitude2 = latitude*Math.PI / 180;
+       // double longitude2 = longitude*Math.PI / 180;
+        //double myLongitude2 = myLatitude * Math.PI / 180;
+        //double myLatitude2 = myLongitude* Math.PI / 180;
+        double latitude2 = 0.5 ;
+        double myLatitude2 = 2 ;
+        double longitude2 = 1 ;
+        double myLongitude2 = 3;
+        double R = 6371 ;
+        distance = R * Math.acos(Math.cos(latitude2) * Math.cos(myLatitude2) *
+                Math.cos(longitude2 - myLongitude2) + Math.sin(latitude2) *
+                Math.sin(myLatitude2));
+        Log.d("TEST",Double.toString(distance));
+        return distance ;
     }
 }
+
